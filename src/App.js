84987f4c79a471d6e1481ef08eae910a1171c0d3
Andrew S <andrews@users-iMac.local>
2020-12-30 01:28:18 -0800

    feat: adding redux, restuc file for naming conv

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './features/counter/Header';
-import SideBar from './features/counter/SideBar';
-import Feed from './features/counter/Feed';
+import Header from './features/Header';
+import SideBar from './features/SideBar';
+import Feed from './features/Feed';
 
 function App() {
   return (
